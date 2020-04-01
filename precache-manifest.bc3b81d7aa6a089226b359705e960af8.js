9f5edab87bdd201ebc879bdc35fbaf15ea69c1e2
dapplion <guillem.lion.benosa@gmail.com>
2020-04-01 10:25:37 +0200

    Updates

---
+++ precache-manifest.bc3b81d7aa6a089226b359705e960af8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc443f5ec9d95126ff7aadc34729a07c",
+    "url": "/dappnode-fallback-ui/index.html"
+  },
+  {
+    "revision": "cb561085ab57c2e7392b",
+    "url": "/dappnode-fallback-ui/static/css/main.11cb5030.chunk.css"
+  },
+  {
+    "revision": "2c9065920d0fa3d2a38e",
+    "url": "/dappnode-fallback-ui/static/js/2.4a9581d8.chunk.js"
+  },
+  {
+    "revision": "346a6e8bff0a06e27b4d2e4150ab7a2b",
+    "url": "/dappnode-fallback-ui/static/js/2.4a9581d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb561085ab57c2e7392b",
+    "url": "/dappnode-fallback-ui/static/js/main.a3ba18b0.chunk.js"
+  },
+  {
+    "revision": "b774f4babf0d8a0326e4",
+    "url": "/dappnode-fallback-ui/static/js/runtime-main.f457f343.js"
+  },
+  {
+    "revision": "e73c6e35dbc00cc6a4c698baa561d5b6",
+    "url": "/dappnode-fallback-ui/static/media/dappNodeAnimation.e73c6e35.gif"
+  }
+]);
